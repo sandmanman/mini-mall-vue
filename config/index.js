018436be7b72333702d96d46f819125e82296435
sandmanman <sandmanman@qq.com>
2017-02-23 16:20:12 +0800

    使用 玩物店 数据

--- config/index.js
+++ config/index.js
@@ -1,9 +1,6 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 var path = require('path')
 
-// 数据来源 https://enjoy.ricebook.com/
-const apiHost = 'https://api.ricebook.com/';
-
 module.exports = {
   // 构建产品时使用的配置
   build: {
@@ -48,60 +45,11 @@ module.exports = {
     // 请求代理表，在这里可以配置特定的请求代理到对应的API接口
     // 例如将'/api/xxx'代理到'www.example.com/api/xxx'
     proxyTable: {
-      '/api/page': { // 首页
-        target: apiHost+'hub/home/v1/home/page.json/',
+      '/api': {
+        target: 'https://ifanr.in/api/v1.4',
         changeOrigin: true,
         pathRewrite: {
-          '^/api/page': ''
-        }
-      },
-      '/api/tab': { // 
-        target: apiHost+'hub/home/v1/home/tab.json/',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api/tab': ''
-        }
-      },
-      '/api/product': { // 产品详细
-        target: apiHost+'3/enjoy_product/product_addition_info.json/',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api/product': ''
-        }
-      },
-      '/api/recommend_product': { // 推荐产品
-        target: apiHost+'3/enjoy_product/recommend_product_v2.json/',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api/recommend_product': ''
-        }
-      },
-      '/api/category': { // 分类
-        target: apiHost+'4/tab/category.json/',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api/category': ''
-        }
-      },
-      '/api/category_product_list': { // 分类列表
-        target: apiHost+'4/tab/category_product_list.json',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api/category_product_list': ''
-        }
-      },
-      '/api/sub_category': { // 列表页排序字段
-        target: apiHost+'4/tab/sub_category.json/',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api/sub_category': ''
-        }
-      },
-      '/api/search': { // 搜索
-        target: apiHost+'3/enjoy_product/search.json/',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api/search': ''
+          '^/api': ''
         }
       }
     },
