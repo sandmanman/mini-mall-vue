794eba866d8f6a683c25599ab7c4d0e1c61e6c33
Qiming <sandmanman@qq.com>
2017-05-02 16:46:20 +0800

    ...

--- src/store/modules/product/index.js
+++ src/store/modules/product/index.js
@@ -1,29 +1,54 @@
 import * as types from '../../mutation-types'
 
+import api from 'src/api/api-conf.js';
+
 // The magic path come from webpack.
-import { _, request } from 'utils'
+import {
+    _,
+    request
+} from 'utils'
 
 export default {
-  state: {
-    items: []
-  },
-  mutations: {
-    [types.RETRIEVE_PRODUCTS] (state, { products }) {
-      state.items = products
+    data() {
+      return {
+        productId: null
+      }
+    },
+    created() {
+        this.getProductId();
+    },
+    state: {
+        items: []
+    },
+    mutations: {
+        [types.RETRIEVE_PRODUCTS](state, {
+            products
+        }) {
+            state.items = products
+        }
+    },
+    getters: {
+        productChunk: state => {
+            return _.chunk(state.items, 4)
+        }
+    },
+    actions: {
+        fetchProducts({
+            commit
+        }) {
+            request.get(api.getProduct(this.productId))
+                .then((res) => {
+                    const products = res.data
+                    commit(types.RETRIEVE_PRODUCTS, {
+                        products
+                    })
+                })
+        }
+    },
+    methods: {
+      getProductId() {
+          console.log('pid'+this.$route.params.id)
+          return this.productId = this.$route.params.id;
+      }
     }
-  },
-  getters: {
-    productChunk: state => {
-      return _.chunk(state.items, 4)
-    }
-  },
-  actions: {
-    fetchProducts ({ commit }) {
-      request.get('/mock/products.json')
-        .then((res) => {
-          const products = res.data
-          commit(types.RETRIEVE_PRODUCTS, { products })
-        })
-    }
-  }
 }
