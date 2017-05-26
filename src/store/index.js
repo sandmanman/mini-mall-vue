781eda8841b9a01991852355f7674e1c410b558c
Qiming <sandmanman@qq.com>
2017-05-26 23:45:35 +0800

    ...

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex';
 
 import cart from './modules/cart';
 import products from './modules/products';
-import auth from './modules/auth';
+
 import messages from './modules/messages';
 
 import * as actions from './actions';
@@ -17,21 +17,6 @@ export default new Vuex.Store({
     modules: {
         cart,
         products,
-        auth,
         messages
     }
 });
-
-
-
-/**
- * vuex相关资料参考
- * 加深对vuex的理解
- * 
- * vuex 的dispatch和commit提交mutation的区别
- * https://segmentfault.com/q/1010000008546097
- * 
- * vuex入门实例
- * https://segmentfault.com/a/1190000005018970
- * 
- */
